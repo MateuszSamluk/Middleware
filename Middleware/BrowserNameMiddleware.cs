8d26ce5e75f5f0483a5d76f9e540aa75b7ab222e
MateuszSamluk <mat.sam00@wp.pl>
2021-04-26 11:54:50 +0200

    Dodaj pliki projektów.

---
+++ Middleware/BrowserNameMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class BrowserNameMiddleware
+    {
+        private RequestDelegate next;
+
+        public BrowserNameMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, IBrowserDetector detector)
+        {
+            var browser = detector.Browser;
+
+            if (browser.Name == BrowserNames.Edge | browser.Name == BrowserNames.EdgeChromium 
+                | browser.Name == BrowserNames.InternetExplorer)
+            {
+                await httpContext.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+            }
+            else
+            {
+                await this.next.Invoke(httpContext);
+            }
+        }
+    }
+}
